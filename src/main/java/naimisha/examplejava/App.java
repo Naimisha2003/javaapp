ac27582fbac751ed085d4a5ab85e21a314f3b913
naimisha <naimisha1704@gmail.com>
2024-01-18 09:51:56 +0530

    javaapp push

---
+++ src/main/java/naimisha/examplejava/App.java
@@ -0,0 +1,13 @@
+package naimisha.examplejava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
